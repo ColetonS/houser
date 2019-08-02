2445eb44c7c63247d9125a9e24cbadac51e33a48
ColetonS <coleton.m.smith@gmail.com>
2019-08-02 13:21:33 -0600

    finished 1.2

--- src/components/Wizard/Wizard.js
+++ src/components/Wizard/Wizard.js
@@ -3,13 +3,36 @@ import React, {Component} from 'react'
 export default class Wizard extends Component {
     constructor() {
         super()
+        
+        this.state = {
+            propertyName: '',
+            address: '',
+            city: '',
+            state: '',
+            zip: 0
+        }
+    }
 
+    handleChange = e => {
+        this.setState({
+            [e.target.name]: e.target.value
+        })
     }
 
     render() {
         return (
             <div>
                 <h1>Wizard</h1>
+                <h2>Property Name</h2>
+                <input onChange={e => this.handleChange(e)} type='text' name='propertyName'/>
+                <h2>Address</h2>
+                <input type='text' onChange={e => this.handleChange(e)} name='address'/>
+                <h2>City</h2>
+                <input type='text' onChange={e => this.handleChange(e)} name='city'/>
+                <h2>State</h2>
+                <input type='text' onChange={e => this.handleChange(e)} name='state'/>
+                <h2>Zip</h2>
+                <input type='number' onChange={e => this.handleChange(e)} name='zip'/>
             </div>
         )
     }
